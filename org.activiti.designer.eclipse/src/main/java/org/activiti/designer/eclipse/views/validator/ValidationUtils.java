c0a740b2da2ca205655d4419e0b483954c904bcb
Juraj Husar <jurosh@gmail.com>
2014-03-29 19:08:21 +0100

    corrected way of getting active editor and diagram, corrected way of selecting+searching shapes

--- org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/views/validator/ValidationUtils.java
+++ org.activiti.designer.eclipse/src/main/java/org/activiti/designer/eclipse/views/validator/ValidationUtils.java
@@ -15,11 +15,14 @@ import org.activiti.designer.util.ActivitiConstants;
 import org.activiti.designer.util.editor.BpmnMemoryModel;
 import org.activiti.designer.util.editor.ModelHandler;
 import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.graphiti.mm.pictograms.ContainerShape;
 import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.MessageBox;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
 
 /**
  * Validation utils
@@ -48,7 +51,7 @@ public class ValidationUtils {
 			Logger.logDebug("Validator resolved");
 
 			// TODO find better way of getting active diagram
-			ValidationResults validateDiagram = processValidator.validateDiagram(ActivitiDiagramEditor.EDITOR.getDiagramTypeProvider().getDiagram());
+			ValidationResults validateDiagram = processValidator.validateDiagram(getActiveDiagram());
 
 			Logger.logInfo("Validation Finished!");
 
@@ -64,24 +67,24 @@ public class ValidationUtils {
 	 * @return
 	 */
 	public static Shape selectShapeByElementId(String id) {
-		ActivitiDiagramEditor editor = ActivitiDiagramEditor.EDITOR;
-		Diagram diagram = editor.getDiagramTypeProvider().getDiagram();
-		
+		ActivitiDiagramEditor editor = getActiveEditor();
+		Diagram diagram = getActiveDiagram();
+
 		Shape findShapeById = findShapeById(id, diagram);
-		
-		if(findShapeById != null) {
-			editor.selectPictogramElements(new PictogramElement[] {findShapeById});
+
+		if (findShapeById != null) {
+			editor.selectPictogramElements(new PictogramElement[] { findShapeById });
 		} else {
 			// show error dialog
-			MessageBox dialog =  new MessageBox(ActivitiPlugin.getShell(), SWT.ICON_ERROR | SWT.OK );
+			MessageBox dialog = new MessageBox(ActivitiPlugin.getShell(), SWT.ICON_ERROR | SWT.OK);
 			dialog.setText("Element not found in Diagram");
-			dialog.setMessage("Element with id `"+id+"` was not found in diagram. Didn't you remove it already?");
-			dialog.open(); 
+			dialog.setMessage("Element with id `" + id + "` was not found in diagram. Didn't you remove it already?");
+			dialog.open();
 		}
-		
+
 		return null;
 	}
-	
+
 	/**
 	 * Find shape in diagram by ID
 	 * @param id
@@ -89,33 +92,89 @@ public class ValidationUtils {
 	 * @return return shape object or <i>null</i> if not found
 	 */
 	public static Shape findShapeById(String id, Diagram diagram) {
+		
 		for (Shape shape : diagram.getChildren()) {
-			Object executionListenerBO = getExecutionListenerBO(shape, diagram);
-			if(executionListenerBO instanceof BaseElement) {
-				if( ((BaseElement) executionListenerBO).getId().equals(id)) {
-					// have correct element, select it
-					return shape;
-				}
+			Shape searchChildren = searchChildren(shape, id, diagram);
+			if(searchChildren != null) {
+				return searchChildren;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Recursively search all elements by ID
+	 * @param shape
+	 * @param id
+	 * @param diagram
+	 * @return
+	 */
+	private static Shape searchChildren(Shape shape, String id, Diagram diagram) {
+
+		Shape targetShape = null;
+		
+		// search recursively
+		if (shape instanceof ContainerShape) {
+			List<Shape> childrens = ((ContainerShape) shape).getChildren();
+			for (Shape shape2 : childrens) {
+				targetShape = searchChildren(shape2, id, diagram);
+			}
+		}
+
+		// check every leaf
+		if (targetShape == null) {
+			// check if not equals
+			BaseElement elem = getBaseElemFromPictogram(shape, diagram);
+			if (elem != null && elem.getId().equals(id)) {
+				// have correct element, select it
+				return shape;
+			}
+		}
+
+		return targetShape;
+	}
+
 	/**
 	 * Get object from diagram
 	 * @param pe
 	 * @param diagram
 	 * @return
 	 */
-	private static Object getExecutionListenerBO(PictogramElement pe, Diagram diagram) {
+	private static BaseElement getBaseElemFromPictogram(PictogramElement pe, Diagram diagram) {
 		BpmnMemoryModel model = ModelHandler.getModel(EcoreUtil.getURI(diagram));
-		Object bo = null;
-		if (pe instanceof Diagram) {
-			bo = model.getBpmnModel().getMainProcess();
-		} else {
-			bo = model.getFeatureProvider().getBusinessObjectForPictogramElement(pe);
+
+		//		Object bo = null;
+		//		if (pe instanceof Diagram) {
+		//			bo = model.getBpmnModel().getMainProcess();
+		//		} else {
+		Object bo = model.getFeatureProvider().getBusinessObjectForPictogramElement(pe);
+		//		}
+		if (bo instanceof BaseElement) {
+			return (BaseElement) bo;
 		}
-		return bo;
+		return null;
+	}
+
+	/**
+	 * Get active editor or return <i>null</i>
+	 * @return
+	 */
+	private static ActivitiDiagramEditor getActiveEditor() {
+		IEditorPart activeEditor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+		if (activeEditor instanceof ActivitiDiagramEditor) {
+			Logger.logDebug("get Activiti Editor, found: " + activeEditor.getTitle());
+			return (ActivitiDiagramEditor) activeEditor;
+		}
+		return null;
+	}
+
+	/**
+	 * Get active diagram
+	 * @return
+	 */
+	private static Diagram getActiveDiagram() {
+		ActivitiDiagramEditor activeEditor = getActiveEditor();
+		return activeEditor == null ? null : getActiveEditor().getDiagramTypeProvider().getDiagram();
 	}
 
 }
