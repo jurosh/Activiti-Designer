84e549473b9f87ae26e56a9c0eb2713c7435849e
Juraj Husar <jurosh@gmail.com>
2014-04-29 11:08:56 +0200

    new rule implemented

--- org.activiti.designer.validation.bpmn20/src/main/java/org/activiti/designer/validation/bpmn20/validation/worker/rules/Level2StartValidationWorker.java
+++ org.activiti.designer.validation.bpmn20/src/main/java/org/activiti/designer/validation/bpmn20/validation/worker/rules/Level2StartValidationWorker.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import org.activiti.bpmn.model.BaseElement;
 import org.activiti.bpmn.model.ExtensionElement;
+import org.activiti.bpmn.model.FlowElement;
 import org.activiti.bpmn.model.SequenceFlow;
 import org.activiti.bpmn.model.StartEvent;
+import org.activiti.bpmn.model.SubProcess;
 import org.activiti.designer.validation.bpmn20.validation.util.BpmnMessageUtils;
 import org.activiti.designer.validation.bpmn20.validation.worker.AbstractAdvancedValidatorWorker;
 import org.eclipse.core.resources.IMarker;
@@ -18,13 +20,12 @@ import org.eclipse.core.resources.IMarker;
  * 
  *  s1 - F
  *  s2
- *  
+ *  s5
+ *    
  * Not implmented:
  *  
  *  s3
  *  s4
- *  s5
- *  s6
  *  
  * 
  * @author Juraj Husar (jurosh@jurosh.com)
@@ -34,6 +35,8 @@ public class Level2StartValidationWorker extends AbstractAdvancedValidatorWorker
 
   private static final String MESSAGE_1 = "/s1/ %s shouldn't have incomming flow.";
   private static final String MESSAGE_2 = "/s2/ %s shouldn't have outgoing message flow.";
+                                            // start event in subprocess
+  private static final String MESSAGE_5 = "/s5/ %s in %s must have none trigger.";
   
 	@Override
 	public void validate() {
@@ -79,8 +82,14 @@ public class Level2StartValidationWorker extends AbstractAdvancedValidatorWorker
 		/**
 		 *  [s5] A start event in a subprocess must have a None trigger. [Exceptions, not part of Level 2 pallete: event subprocess start event].
 		 */
-	   // TODO bpmn20 rule
-
+    List<SubProcess> subprocesses = getNodes(SubProcess.class);
+    for (SubProcess subProcess : subprocesses) {
+      for (FlowElement flowElement : subProcess.getFlowElements()) {
+        if (flowElement instanceof StartEvent && ((StartEvent) flowElement).getEventDefinitions().size() > 0) {
+          createErr(IMarker.SEVERITY_ERROR, String.format(MESSAGE_5, formatName(flowElement), formatName(subProcess)), flowElement);
+        }
+      }
+    }
 		
 	}
 
